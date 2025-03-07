03d8b547db683aa8ef93f2f4e8c97f316f6710f8
vitor henkels <vitor.henkels@gmail.com>
2025-03-07 09:13:00 -0300

    primeira alteração

---
+++ COMANDOS DE CLONE GIT.sh
@@ -0,0 +1,14 @@
+COMANDOS DE CLONE
+
+git clone<caminho> = clona um projeto criando a pasta com o mesmo nome do repositório remoto
+git clone <caminho> pasta = clona um repositório criando a pasta definida no final do COMANDO
+git clone<caminho> = clona um repositório diretamente dentro da pasta que está sem criar a pasta com o nome do repositório remoto
+
+//navegar entre pastas
+
+cd<caminho> = entra em um diretório
+cd ../ = volta uma ou mais pastas ../ uma pasta ../../ duas pastas e assim em diante.
+cd ~ = vai para a pasta principal do usuario.
+
+LS = lista de diretórios e arquivos que estão presentes no caminho atual
+ls -a = lista itens ocultos também
